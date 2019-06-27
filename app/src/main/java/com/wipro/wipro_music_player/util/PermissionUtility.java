b0c5a55ba56247d1795e11db135a611a6db450d1
Artur Torres <artur.sukiennik82@gmail.com>
2019-06-27 10:52:33 +0100

    Permission Utility class updated - code modulirized

--- app/src/main/java/com/wipro/wipro_music_player/util/PermissionUtility.java
+++ app/src/main/java/com/wipro/wipro_music_player/util/PermissionUtility.java
@@ -11,26 +11,29 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class PermissionUtility {
+    private final static String PERMISSION_TAG = "PERMISSION_TAG";
+    private final static String PERMISSION_MSG = "Storage Permission Accepted!";
+
     // Self-check permissions
     @SuppressLint("ObsoleteSdkInt")
     public static void checkStoragePermissions(Context context, Activity activity) {
-        final String PERMISSION_TAG = "PERMISSION_TAG";
-        final String PERMISSION_MSG = "Storage Permission Accepted!";
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (context.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(context, PERMISSION_MSG, Toast.LENGTH_SHORT).show();
-                Log.i(PERMISSION_TAG, PERMISSION_MSG);
+                showToastAndLogMessages(context);
             } else {
                 ActivityCompat.requestPermissions(activity, new String[] {
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         Manifest.permission.READ_EXTERNAL_STORAGE }, 1);
                 Log.i(PERMISSION_TAG,"Permission is Revoked!");
             }
+        } else {
+            showToastAndLogMessages(context);
         }
-        else {
-            Toast.makeText(context, PERMISSION_MSG, Toast.LENGTH_SHORT).show();
-            Log.i(PERMISSION_TAG, PERMISSION_MSG);
-        }
+    }
+
+    // Put Toast and Log messages together and reuse it across the block statements in check permission method
+    private static void showToastAndLogMessages(Context context) {
+        Toast.makeText(context, PERMISSION_MSG, Toast.LENGTH_SHORT).show();
+        Log.i(PERMISSION_TAG, PERMISSION_MSG);
     }
 }
