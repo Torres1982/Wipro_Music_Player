9dd26b9e365e9a2e47da7156d2b3becd4a6db588
Artur Torres <artur.sukiennik82@gmail.com>
2019-07-16 10:11:54 +0100

    New Real Configuration Settings applied, so it can handle the new Schema Version in case of Migration.

--- app/src/main/java/com/wipro/wipro_music_player/PlayerApplication.java
+++ app/src/main/java/com/wipro/wipro_music_player/PlayerApplication.java
@@ -2,6 +2,8 @@ package com.wipro.wipro_music_player;
 
 import android.app.Application;
 import android.util.Log;
+
+import io.realm.DynamicRealm;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -10,29 +12,35 @@ public class PlayerApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
-        RealmConfiguration config = new RealmConfiguration.Builder().name(Constants.RealmDB.DEFAULT_REALM_CONFIGURATION_NAME).build();
+        //RealmConfiguration config = new RealmConfiguration.Builder().name(Constants.RealmDB.DEFAULT_REALM_CONFIGURATION_NAME).build();
+        RealmConfiguration config = createDefaultRealmConfiguration();
         Realm.setDefaultConfiguration(config);
         Log.i(Constants.LogTags.MUSIC_TAG, "Realm has been initiated from the Application class!");
-    }
-}
 
-// Set up the Default Realm Configuration for the Application
-//    private static RealmConfiguration createDefaultRealmConfiguration() {
-//        // Generate the random Encryption Key
+        getCurrentSchemaVersion(config);
+    }
+
+    // Set up the Default Realm Configuration for the Application
+    private static RealmConfiguration createDefaultRealmConfiguration() {
+        // Generate the random Encryption Key
 //        byte [] encryptionKey = new byte [64];
 //        new SecureRandom().nextBytes(encryptionKey);
-//
-//        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
-//                .name(Constants.RealmDB.DEFAULT_REALM_CONFIGURATION_NAME)
-//                .encryptionKey(encryptionKey)
-//                .schemaVersion(0)
-//                .deleteRealmIfMigrationNeeded()
-//                .build();
-//        Realm.setDefaultConfiguration(realmConfiguration);
-//        return realmConfiguration;
-//    }
 
-// Get the instance of Realm
-//    private static Realm getRealmInstance() {
-//        return Realm.getInstance(createDefaultRealmConfiguration());
-//    }
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name(Constants.RealmDB.DEFAULT_REALM_CONFIGURATION_NAME)
+//                .encryptionKey(encryptionKey)
+                .schemaVersion(1)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+        return realmConfiguration;
+    }
+
+    // Retrieve the Current Schema Version
+    private static void getCurrentSchemaVersion(RealmConfiguration config) {
+        DynamicRealm dynRealm = DynamicRealm.getInstance(config);
+        long version = dynRealm.getVersion();
+        Log.i(Constants.LogTags.MUSIC_TAG, "Current Schema Version: " + version);
+        dynRealm.close();
+    }
+}
